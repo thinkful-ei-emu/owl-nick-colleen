d21041f8bdc6aa0cd2caee808b03a9b605dbd954
Colleen <colleenhiggins87@gmail.com>
2019-06-25 09:46:22 -0700

    app set up

---
+++ owl/src/Participant.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//props: name, avatar, insession, on stage, ID 
+function Participant (props) {
+return <div className="participant">
+  <img src={props.avatar}
+</div>
+}
